1dadf4845f387b8903e3d757c40cea4771ea2369
AknoTech <158369442+AknoTech@users.noreply.github.com>
2025-11-05 15:12:37 -0300

    update

--- minhaliga/urls.py
+++ minhaliga/urls.py
@@ -1,9 +1,16 @@
-# Este é o arquivo minhaliga/urls.py
+# Este é o NOVO E CORRIGIDO arquivo minhaliga/urls.py
 
 from django.contrib import admin
-from django.urls import path, include  # Adicione o "include" aqui
+from django.urls import path, include
+from django.conf import settings             # <-- ADICIONE ESTA LINHA
+from django.conf.urls.static import static # <-- ADICIONE ESTA LINHA
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('liga.urls')),  # <-- ADICIONE ESTA LINHA
+    path('', include('liga.urls')),
 ]
+
+# Esta linha mágica diz ao Django para servir
+# os arquivos de /media/ quando DEBUG = False
+if not settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
